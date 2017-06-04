866baaac01b0bad8b206342d1a056a0c26e4b952
JustPilz <dkrivyakov@gmail.com>
2017-06-05 01:03:53 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# Underline any text in your docs
+
+### Install
+
+Add the below to your `book.json` file, then run `gitbook install`:
+
+```json
+{
+    "plugins": ["underline"]
+}
+```
+
+### Usage
+
+Just add __ before & after your desired text to underline! I'll show you some examples:
+
+<u>underline</u> = \_\_underline\_\_  
+<u>*underline italics*</u> = \_\_\*underline italics\*\_\_  
+<u>**underline bold**</u> = \_\_\*\*underline bold\*\*\_\_  
+<u>***underline bold italics***</u> = \_\_\*\*\*underline bold italics\*\*\*\_\_
---
+++ index.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+    hooks: {
+        "page:before": function (page) {
+            function triple(str, prefix, text, postfix, offset, s) {
+              return '<em><u>' + text + '</u></em>';
+            }
+            function double(str, prefix, text, postfix, offset, s) {
+              return '<u>' + text + '</u>';
+            }
+            page.content = page.content.replace( /(___)(.*?)(___)/gi, triple);
+            page.content = page.content.replace( /(__)(.*?)(__)/gi, double);
+            return page;
+        }
+    }
+};
